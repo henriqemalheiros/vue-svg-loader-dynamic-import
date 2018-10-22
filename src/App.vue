5fa7ccdc05acc39b5dac468a2fce7c1f791637c2
Henrique Malheiros <henriqemalheiros@gmail.com>
2018-10-22 17:49:26 -0200

    First commit

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <DynamicIcon icon="mood_bad"/>
+    <DynamicIcon icon="mood"/>
+  </div>
+</template>
+
+<script>
+import DynamicIcon from './components/DynamicIcon.vue'
+
+export default {
+  name: 'IconList',
+  components: {
+    DynamicIcon
+  }
+}
+</script>
---
+++ src/components/DynamicIcon.vue
@@ -0,0 +1,23 @@
+<template>
+  <component :is="dynamicIcon"/>
+</template>
+
+<script>
+export default {
+  name: 'DynamicIcon',
+  props: {
+    icon: {
+      type: String,
+      required: true
+    }
+  },
+  computed: {
+    dynamicIcon () {
+      return () => import(
+        /* webpackChunkName: "icons/icon-[request]" */
+        `../assets/icons/${this.icon}.svg`
+      )
+    }
+  }
+}
+</script>
